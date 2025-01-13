f675a4e41779eda8144cc2165acff4b04642568d
Girish joshi <158801722+Girish14j@users.noreply.github.com>
2025-01-13 17:34:49 +0530

    Delete my-app directory

--- my-app/src/components/Card.jsx
+++
@@ -1,5 +0,0 @@
-export default function Greeting(props){
-    return(
-        <h1>heyyy...how are youu..{props.name}</h1>
-    )
-}
--- my-app/src/components/Greetingcard.jsx
+++
@@ -1,9 +0,0 @@
-export default function Greetingcard(props){
-    return(
-        <div className="card">
-            <h1>{props.title}</h1>
-            <h2>{props.name}</h2>
-            <p>{props.message}</p>
-        </div>
-    )
-}
